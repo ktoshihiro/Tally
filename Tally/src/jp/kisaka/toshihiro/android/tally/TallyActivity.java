a3da358c5bee12f6246b18f9452b351f0f033d4c
KISAKA Toshihiro <ktoshihiro@gmail.com>
2011-12-20 16:13:24 +0900

    first commit

---
+++ Tally/src/jp/kisaka/toshihiro/android/tally/TallyActivity.java
@@ -0,0 +1,15 @@
+package jp.kisaka.toshihiro.android.tally;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TallyActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
